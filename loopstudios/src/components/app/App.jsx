035b54a4c0450d4620b0a2a0aacfcb4eae1a60d6
mario-cartagena <jhonmariocartagena@gmail.com>
2023-05-04 15:17:04 -0500

    funcionalidad de pintar las cards con .map()

--- loopstudios/src/components/app/App.jsx
+++ loopstudios/src/components/app/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../header/Header';
 import GlobalStyles from "./StyleApp";
+import Main from '../main/Main';
 
 const App = () => {
   return (
    <>
    <GlobalStyles/>
    <Header/>
+   <Main/>
    </>
   )
 }
---
+++ loopstudios/src/components/main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainCreation from '../mainCreation/MainCreation';
+
+const Main = () => {
+  return (
+    <main>
+        <MainCreation/>
+    </main>
+  )
+}
+
+export default Main;

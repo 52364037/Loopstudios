39b433e704e6066da3388ec0deb6538eaeddf8e9
Maria Antonieta Pereira <antonietap0@gmail.com>
2023-05-04 16:16:34 -0500

    edit style in h1 on header

--- loopstudios/src/components/header/StyleHeader.jsx
+++ loopstudios/src/components/header/StyleHeader.jsx
@@ -1,18 +1,30 @@
 import styled from 'styled-components';
 import imgHeader from '../../assets/img/imagen-header.jpg';
 export const StyleImage = styled.div`
-background-image: url(${imgHeader});
-background-size: cover;
-height: 650px;
-h1 {
-  border: 1px solid #fff;
-  color: #fff;
-  width: 35%;
-  font-family: 'Alata', sans-serif !important;
-  font-weight: 300;
-  font-size: 45px;
-  padding: 2rem;
-  margin-left: 10rem;
-  margin-top: 5rem;
-}
+  background-image: url(${imgHeader});
+  background-size: cover;
+  height: 650px;
+  h1 {
+    border: 1px solid #fff;
+    display: flex;
+    align-items: center;
+    color: #fff;
+    width: 45%;
+    font-family: 'Alata', sans-serif !important;
+    font-weight: 300;
+    font-size: 45px;
+    padding: 2rem;
+    margin-left: 10rem;
+    margin-top: 5rem;
+  }
+      /* Media query for 375px */
+      @media (max-width: 375px) {
+        h1 {
+          font-size: 35px;
+          margin-left: 10px;
+          margin-right: 10px;
+          width: auto;
+          margin-top: 9rem;
+        }
+      }
 `;

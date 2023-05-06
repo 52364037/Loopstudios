2723b0fba77b5ec7d0c5baa730c143c6634e82d7
Yolanda Vásquez <yolandavasquezhuer@hotmail.com>
2023-05-06 10:02:00 -0500

    Estilos y responsive del MainInterctive

--- loopstudios/src/components/mainInteractive/StyledMainInteractive.jsx
+++ loopstudios/src/components/mainInteractive/StyledMainInteractive.jsx
@@ -1,5 +1,7 @@
+ 
 import styled from 'styled-components';
 import ImgStyledMainInterctive from '../../assets/img/thumbnail.jpg';
+
 
 export const StyleImageInteractive = styled.div`
 background-image: url(${ImgStyledMainInterctive});
@@ -10,19 +12,41 @@ margin: 120px;
 position: relative;
 `;
 export const StyleTextoInteractive = styled.div`
-   
+    padding: 6rem;
+    display: block;
+    justify-content: center;
+    max-width: 30rem;
     background: white;
-    width: 450px;
-    height: 310px;
-    border: 3px solid;
+    box-sizing: content-box;
+    height: auto;
     position: relative;
     top: -430px;
     left: 750px;
     h2 {
+        display: flex;
+        text-align: start;
+        font-weight: 10px; 
+        font-family: 'Josefin Sans', sans-serif;
         font-size: 50px;
+        margin-bottom: 1rem; 
+        line-height: 1;  
+        color: #212529;
+        
     }
+    p {
+        font-family: 'Alata', sans-serif;
+        color: #696969;
+    }
+/* Media query for 375px */
+@media (max-width: 375px) {
+      display: block;
+      top: -50px;
+      left: 180px;
+      padding: 70px;
 
+}
 `;
+
 
 
 
